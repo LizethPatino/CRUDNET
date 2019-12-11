7eb84b8580decbb09d7dc79114a785e92b37422d
Lizeth Patiño <lizeth0330@outlook.com>
2019-12-11 09:35:22 -0500

    Creacion controlador y modificacion route

---
+++ CrudEjemplo/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc; 
+ 
+namespace CrudEjemplo.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "Este es el método <b>Por defecto</b> en acción...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
